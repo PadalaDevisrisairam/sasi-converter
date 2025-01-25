291202a77ef6d0ff6e90892dc3a123279eedba49
sai <devisri.padala@sasi.ac.in>
2025-01-25 22:33:33 +0530

    version 3.1.0

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.get('/convert', (req, res) => {
         res.status(500).json({ error: e });
     }
 });
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
